389865f4af060911dc0b35adcea4548809df04de
Joaco Viola <j2001viola@gmail.com>
2024-02-19 18:03:03 -0300

    dsfa

--- estimaciones.R
+++ estimaciones.R
@@ -1,2 +1,24 @@
 # estimaciones de variables de interes
 
+# Le agregamos la variable de id_hogar a los datos con la respuesta de las variables de interes para poder
+# asignar a cada hogar calibrado una respuesta de la encuesta.
+
+# primero filtraremos los datos de las respuesta a las variables que nos interesa junto con la variable de identificacion
+
+datos_identificados <- datos_identificados[c(1,30,40)]
+
+colnames(datos_identificados) <- c("id_hogar","Recicla","ConoceLaPaloma")
+
+
+datos_identificados <- datos_identificados %>% mutate(
+  Recicla = as.factor(Recicla),
+  ConoceLaPaloma = as.factor(ConoceLaPaloma)
+)
+
+
+levels(datos_identificados$Recicla)
+levels(datos_identificados$ConoceLaPaloma)
+
+
+
+
